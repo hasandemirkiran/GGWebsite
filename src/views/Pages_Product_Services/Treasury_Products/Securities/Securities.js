d10055d2e8ae2ee6dabb7d3eb81c405e097407a8
hasandemirkiran <43812957+hasandemirkiran@users.noreply.github.com>
2021-04-14 13:54:27 +0300

    Redunduncies deleted

--- src/views/Pages_Product_Services/Treasury_Products/Securities/Securities.js
+++ src/views/Pages_Product_Services/Treasury_Products/Securities/Securities.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Divider } from "@material-ui/core";
 
 import { Section, SectionAlternate } from "components/organisms";
 
