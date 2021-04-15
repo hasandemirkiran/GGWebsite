7250a7c5e731b30e8dd8c4addaebfd07e3c94502
hasandemirkiran <43812957+hasandemirkiran@users.noreply.github.com>
2021-04-15 14:39:56 +0300

    delete react-pdf

--- src/views/Pages_Product_Services/Others/ProductandServicesPrice/ProductandServicesPrice.js
+++ src/views/Pages_Product_Services/Others/ProductandServicesPrice/ProductandServicesPrice.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Section, SectionAlternate } from "components/organisms";
 import { Process } from "./components";
 
 //show pdf
 import samplePDF from "../../../../assets/PDFs/urunvemusterihiz.pdf";
+import { Document, Page, pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,9 +22,26 @@ const useStyles = makeStyles((theme) => ({
 const ProductandServicesPrice = () => {
   const classes = useStyles();
 
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
   return (
     <div className={classes.root}>
-      <Section></Section>
+      <Section>
+        <Document
+          file="./urunvemusterihiz.pdf"
+          onLoadSuccess={onDocumentLoadSuccess}
+        >
+          <Page pageNumber={pageNumber} />
+        </Document>
+        <p>
+          Page {pageNumber} of {numPages}
+        </p>
+      </Section>
     </div>
   );
 };
