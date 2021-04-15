793fe60a6c3dd58cd5905f8b2fb847d2aea5652a
hasandemirkiran <43812957+hasandemirkiran@users.noreply.github.com>
2021-04-15 15:10:59 +0300

    Contract Forms page added

--- src/views/Pages_Product_Services/Others/ContractForms/ContractForms.js
+++ src/views/Pages_Product_Services/Others/ContractForms/ContractForms.js
@@ -35,6 +35,58 @@ const ContractForms = () => {
             Bankacilik Hizmetleri Sozlesme Oncesi Bilgilendirme Formu
           </Button>
         </Box>
+
+        <Box display="flex" justifyContent="center" mt={2}>
+          <Button
+            color="primary"
+            component="a"
+            href="https://goldenglobalbank.com.tr/icerik/uploads/2020/03/bankacilik_hizmetleri_sozlesmesi.pdf"
+            variant="contained"
+            target="blank"
+            fullWidth
+          >
+            Bankacilik Hizmetleri Sozlesmesi
+          </Button>
+        </Box>
+
+        <Box display="flex" justifyContent="center" mt={2}>
+          <Button
+            color="primary"
+            component="a"
+            href="https://goldenglobalbank.com.tr/icerik/uploads/2020/06/Genel_Kredi_Sozlesmesi_Sozlesme_Oncesi_Bilgilendirme_Formu.pdf"
+            variant="contained"
+            target="blank"
+            fullWidth
+          >
+            Genel Kredi Sozlesmesi Sozlesme Oncesi Bilgilendirme Formu
+          </Button>
+        </Box>
+
+        <Box display="flex" justifyContent="center" mt={2}>
+          <Button
+            color="primary"
+            component="a"
+            href="https://goldenglobalbank.com.tr/icerik/uploads/2020/06/Genel_Kredi_Sozlesmesi.pdf"
+            variant="contained"
+            target="blank"
+            fullWidth
+          >
+            Genel Kredi Sozlesmesi Sozlesmei
+          </Button>
+        </Box>
+
+        <Box display="flex" justifyContent="center" mt={2}>
+          <Button
+            color="primary"
+            component="a"
+            href="https://goldenglobalbank.com.tr/icerik/uploads/2020/06/Teminatlar_Hakk%C4%B1nda_Bilgilendirme.pdf"
+            variant="contained"
+            target="blank"
+            fullWidth
+          >
+            Teminatlar Hakkinda Bilgilendirme
+          </Button>
+        </Box>
       </Section>
     </div>
   );
